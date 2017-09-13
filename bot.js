7c5d46093a7ecdf5037603da4b288c404f7960db
Hill John-Nathan <john.nat.hill@gmail.com>
2017-09-13 18:44:00 +0200

    Added interaction with a Wemos D1 Mini

--- bot.js
+++ bot.js
@@ -1,22 +1,63 @@
-const Discord = require('discord.js');
-const client = new Discord.Client();
-const prefix = "!";
-const config = require("./config.json");
+const Discord = require('discord.js')
+var mqtt = require('mqtt')
+const client = new Discord.Client()
+const prefix = "!"
+const config = require("./config.json")
+
+var channel
+
+// ============================ MQTT Set up ============================
+var mqttOptions = {
+  host: 'localhost',
+  port: '1883',
+  username: config.mqtt_username,
+  password: config.mqtt_password
+}
+
+const wemos_temperature_topic = 'wemos/temperature'
+const wemos_notification_topic = 'wemos/notification'
+var mqttClient = mqtt.connect(mqttOptions)
+
+mqttClient.on('connect', function () {
+  mqttClient.subscribe(wemos_temperature_topic)
+  mqttClient.publish('presence', 'Hello mqtt')
+})
+
+mqttClient.on('message', function(topic, message) {
+  if (topic === wemos_temperature_topic) {
+    channel.send('Wemos notifies that the temperature is '+ message +' °C')
+  }
+})
+
+mqttClient.on('error', function() {
+  console.log('Failed at connecting to '+ mqttOptions.host)
+})
+
+// ============================ Discord Bot Set up ============================
 
 client.on('ready', () => {
-  console.log('I am ready!');
-});
+  console.log('I am ready!')
+})
 
 client.on('message', message => {
-  if(message.content.indexOf(prefix) !== 0) return;
+  if(message.content.indexOf(prefix) !== 0) return
 
-  const args = message.content.split(/\s+/g);
-  const command = args.shift().slice(prefix.length).toLowerCase();
+  const args = message.content.split(/\s+/g)
+  const command = args.shift().slice(prefix.length).toLowerCase()
 
   if (command === 'ping') {
-    message.reply('pong');
+    message.reply('pong')
   }
 
-});
+  if (command === 'myinfo') {
+    console.log(message.author)
+  }
 
-client.login(config.token);
+  if (command === 'temperature') {
+    channel = message.channel
+    mqttClient.publish(wemos_notification_topic, 'temperature')
+  }
+
+})
+
+client.login(config.token)
